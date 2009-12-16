51bcac27f97b6f5faeec2bf6ecb48c87c6de2e77
Jason Shankel <jpshankel@gmail.com>
2009-12-16 12:15:36 -0800

    * updated flex/bison * added vc2008 support

---
+++ JISPConsole/JISP.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "..\JISPParse\JISPCompile.h"
+#include "..\JISPEvaluate\JISPList.h"
+ 
+int main(int argc,char **argv)
+{
+
+    JISP::JISPListElementUnitTest();
+  
+    std::string str;
+    std::cout << ">";
+    while (getline(std::cin,str))
+    {
+        std::cout << ":";
+        std::cout << str;
+        std::cout << std::endl;
+
+        if (str == "exit") break;
+
+        JISP::CompileString(str.c_str());
+        std::cout << ">";
+    }
+    return 0;
+}
